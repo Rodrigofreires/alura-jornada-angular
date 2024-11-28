0898139fc86af0778059f1c954a22f3f67b8984b
mac-rodrigos <rodrigo.soares@mac.com.br>
2024-11-28 08:42:11 -0300

    feat: projeto completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ModalComponent } from './shared/modal/modal.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BotaoControleComponent } from './shared/botao-controle/botao-controle.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +40,7 @@ import { ModalComponent } from './shared/modal/modal.component';
     CardDepoimentoComponent,
     FormBuscaComponent,
     ModalComponent,
+    BotaoControleComponent,
   ],
   imports: [
     BrowserModule,
@@ -53,6 +56,7 @@ import { ModalComponent } from './shared/modal/modal.component';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
